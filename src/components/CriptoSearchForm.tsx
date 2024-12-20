fa624f83e3ef340e2caf7a58deb7b85987afb36d
Johnny <jonathanbc2012@gmail.com>
2024-12-20 13:50:46 +0100

    feat: criptocurrencies mapped in cryptocurrency option

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,9 @@
+import { useCryptoStore } from "../store";
 import {currencies} from "../data"
 
 const CriptoSearchForm = () => {
+  const cryptocurrencies = useCryptoStore(state=> state.cryptocurrencies)
+
   return (
     <form className="form">
       <div className="field">
@@ -17,6 +20,13 @@ const CriptoSearchForm = () => {
         <label htmlFor="cryptocurrency">Criptomoneda:</label>
         <select name="cryptocurrency" id="cryptocurrency">
           <option value="">-- Seleccione --</option>
+          {cryptocurrencies.map(crypto => (
+            <option
+              key={crypto.CoinInfo.Name}
+              value={crypto.CoinInfo.Name}
+            >{crypto.CoinInfo.FullName}
+            </option>
+          ))}
         </select>
       </div>
 
